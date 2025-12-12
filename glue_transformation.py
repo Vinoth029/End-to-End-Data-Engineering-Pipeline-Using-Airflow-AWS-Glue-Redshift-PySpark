e60550f618225d961f1711e3c8552fa648e8a8b3
Vinoth K <96591377+Vinoth029@users.noreply.github.com>
2025-12-12 13:05:34 +0530

    Update glue_transformation.py

--- glue_transformation.py
+++ glue_transformation.py
@@ -14,12 +14,12 @@ s3_output = sys.argv[2]
 df = spark.read.parquet(s3_input)
 
 df_clean = (
-    df.withColumn('business_key', F.trim('business_key'))
+    df.withColumn('address', F.trim('address'))
       .withColumn('amount', F.col('amount').cast('double'))
       .withColumn('event_date', F.to_date('event_timestamp'))
 )
 
-w = Window.partitionBy("business_key").orderBy(F.col("event_timestamp").desc())
+w = Window.partitionBy("region").orderBy(F.col("event_timestamp").desc())
 df_final = df_clean.withColumn("rn", F.row_number().over(w)).filter("rn = 1").drop("rn")
 
 df_final.write.partitionBy("event_date").mode("overwrite").parquet(s3_output)
