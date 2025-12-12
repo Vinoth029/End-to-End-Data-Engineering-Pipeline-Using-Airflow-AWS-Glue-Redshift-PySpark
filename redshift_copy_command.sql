11dd9d33f59040dacbc96a6876e901f4f98137a0
Vinoth K <96591377+Vinoth029@users.noreply.github.com>
2025-12-12 13:06:13 +0530

    Update redshift_copy_command.sql

--- redshift_copy_command.sql
+++ redshift_copy_command.sql
@@ -1,4 +1,4 @@
-COPY staging_mydataset
-FROM 's3://my-processed-bucket/processed/mydataset/{{ ds_nodash }}/'
+COPY staging_customers
+FROM 's3://processed-bucket/processed/mydataset/{{ ds_nodash }}/'
 IAM_ROLE 'arn:aws:iam::123456789012:role/RedshiftCopyRole'
 FORMAT AS PARQUET;
